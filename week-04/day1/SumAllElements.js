4ee3eac224feb54f5f44e6dc22d474aeab4ff65c
Éva <kulcsareva@gmail.com>
2020-12-02 21:59:58 +0100

    SumAllElements,appendA,Colors,doubleItem,matrix

--- week-04/day1/SumAllElements.js
+++ week-04/day1/SumAllElements.js
@@ -3,11 +3,21 @@
 // Logold ki az elemek összegét a konzolra!
 
 const numbers = [3, 4, 5, 6, 7];
-console.log(numbers.reduce((number,nextNumber)=> number+nextNumber));
+
+let total = 0;
+
+for (let i = 0; i<numbers.length; i++){
+    total += numbers[i];
+};
+
+console.log (total);
 
 
-/*
-const list = [1, 2, 3, 4, 5];
+const list = [3, 4, 5, 6, 7];
 console.log(list.reduce((number, nextNumber) => number + nextNumber));
-// --> 15
-*/
+
+let numbers2 = [3, 4, 5, 6, 7];
+let sum2 = 0;
+numbers2.forEach( number => sum2 += number );
+console.log( sum2 );
+
