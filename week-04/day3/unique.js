4a220e58f2240c5aeba4236ed502f92e45ff9774
Éva <kulcsareva@gmail.com>
2020-12-03 22:26:22 +0100

    unique

---
+++ week-04/day3/unique.js
@@ -0,0 +1,15 @@
+// Hozz létre egy függvényt, ami kap egy számokból álló listát paraméterként
+// Visszad egy számokból álló listát, ahol minden szám pontosan egyszer
+// szerepel
+const unique = [1, 11, 34, 11, 52, 61, 1, 34];
+
+let uniqueCut = [];
+function unique(arr) {
+    for (i = 0; i<=unique.length; i++){
+        if (number)
+    }
+}
+
+//  Példa
+console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]))
+//  ezt kéne kiírnia: `[1, 11, 34, 52, 61]`
