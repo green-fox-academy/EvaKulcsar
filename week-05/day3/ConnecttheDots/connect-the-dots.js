93db0d0e644eb639e0dd75586585a309c22e56e9
Éva <kulcsareva@gmail.com>
2020-12-06 16:21:02 +0100

    connectTheDots

---
+++ week-05/day3/ConnecttheDots/connect-the-dots.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas');
+const ctx = canvas.getContext('2d');
+
+// NE NYÚLJ A FENTI KÓDHOZ
+
+// Készíts egy függvényt, ami egy paramétert vár:
+// Egy listát [x, y] pontokkal
+// és összeköti őket zöld vonalakkal.
+// Kösd össze, kapsz egy dobozt: [[10, 10], [290, 10], [290, 290], [10, 290]]
+// Kösd össze ezeket: [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70],
+// [120, 100], [85, 130], [50, 100]]
+
+ [[10, 10], [290, 10], [290, 290], [10, 290]]
+[[50, 100], [70, 70], [80, 90], [90, 90], [100, 70], [120, 100], [85, 130], [50, 100]]
+
+function box (x,y){
+    ctx.beginPath();
+}
