fb2ae05a57602100b3eaceeafb64eced05ccd51a
Éva <kulcsareva@gmail.com>
2020-12-07 00:51:35 +0100

    sharpie

---
+++ week-05/day5/sharpie.js
@@ -0,0 +1,35 @@
+"use strict";
+
+//Készíts egy Sharpie osztályt
+//Tudnunk kell mindegyik filctoll színét color (ez legyen string típusú), szélességét width 
+//(legyen egy lebegőpontos szám), a tinta mennyiségét inkAmount (szintén egy lebegőpontos szám)
+//Példányosításkor a color és a width legyenek kötelezően megadandóak
+//Minden példányosított filctoll inkAmount értéke legyen alapból 100
+//Legyen az objektumoknak egy use() (használ) metódusa, ami csökkenti az inkAmount értékét
+
+class Sharpie {
+    color = "";
+    width = 0;
+    inkAmount = 100;
+
+    constructor (color, width){
+
+        this.color = color;
+        this.width = width;
+
+    }
+
+    use(){
+        this.inkAmount --;
+    }
+}
+
+const feltTipPen = [
+    new Sharpie ("red",20),
+    new Sharpie ("black",30),
+    new Sharpie ("blue",10),
+];
+
+feltTipPen.use();
+
+console.log (feltTipPen[0]);
