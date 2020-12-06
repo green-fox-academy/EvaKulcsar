6914c31aa98cd4094e11b15aebd4792b8c6357aa
Éva <kulcsareva@gmail.com>
2020-12-06 15:51:26 +0100

    post-it,colore texts

--- week-05/day5/post-it.js
+++ week-05/day5/post-it.js
@@ -1,17 +1,24 @@
 'use strict';
 
-class PostIt {
+//Készíts egy PostIt osztályt, aminek a következők a tagváltozói:
+//backgroundColor
+//text
+//textColor
+//Készíts néhány példa post-it objektumot:
+//egy narancssárgát kék szöveggel: "Egy, megerett a meggy"
+//egy rózsaszínt fekete szöveggel: "Ketto, csipkebokor vesszo"
+//egy sárgát zöld szöveggel: "1 liter tej!"
 
+
+class PostIt{
     constructor(backgroundColor,text,textColor){
-        this.backgroundColor = backgroundColor;
+    
+        this.beckgroundColor = backgroundColor;
         this.text = text;
-        this.textcolor = textColor;
+        this.textColor = textColor;
     }
-
-    backgroundColor(color){
-        this.color = this.color
-    }
-
 }
 
-
+console.log (new PostIt("orange","blue","Egymegérett a meggy"));
+console.log (new PostIt("pink","black","Ketto,csipkebokor vessző"));
+console.log (new PostIt("yellow","green","1 liter tej!"));
