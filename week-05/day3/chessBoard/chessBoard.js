18358ab2839dc8ddc005a6a779360b98fcae82bf
Éva <kulcsareva@gmail.com>
2020-12-06 16:18:15 +0100

    chessBorard

--- week-05/day3/chessBoard/chessBoard.js
+++ week-05/day3/chessBoard/chessBoard.js
@@ -4,51 +4,28 @@ const canvas = document.querySelector('.main-canvas');
 const ctx = canvas.getContext('2d');
 // Töltsd ki a canvast sakktábla mintával.
 
+/*
+function drawSquare(x,y,width,heigth,color){
 
-ctx.fillRect (0,0,40,40); 
-ctx.fillRect(0,80,40,40);
-ctx.fillRect (0,160,40,40);
-ctx.fillRect (0,240,40,41);
-ctx.fillRect (0,320,40,40);
+    ctx.fillrect = (x,y,width,heigth);
+    ctx.fillStyle = color;
 
-ctx.fillRect (40,40,40,40); 
-ctx.fillRect (40,120,40,40);
-ctx.fillRect (40,200,40,40);
-ctx.fillRect (40,280,40,40);
-ctx.fillRect (40,360,40,40);
+}
 
-ctx.fillRect (80,0,40,40); 
-ctx.fillRect (80,80,40,40,)
-ctx.fillRect(80,160,40,40);
-ctx.fillRect (80,240,40,40);
-ctx.fillRect (80,320,40,41);
-ctx.fillRect (80,400,40,40);
+*/
+function drawSquare(x, y, width, heigth, color) {
+    ctx.fillRect(x, y, width, heigth);
+    ctx.fillStyle = color;
+}
 
-ctx.fillRect (120,40,40,40); 
-ctx.fillRect (120,120,40,40);
-ctx.fillRect (120,200,40,40);
-ctx.fillRect (120,280,40,40);
-ctx.fillRect (120,360,40,40);
+for (let i = -60; i < 400; i += 120) {
+    for (let j = -60; j < 600; j += 120) {
+        drawSquare(j + 60, i + 60, 60, 60, "black");
+    }
+}
 
-ctx.fillRect (160,0,40,40); 
-ctx.fillRect (160,80,40,40,)
-ctx.fillRect(160,160,40,40);
-ctx.fillRect (160,240,40,40);
-ctx.fillRect (160,320,40,41);
-
-ctx.fillRect (200,40,40,40); 
-ctx.fillRect (200,120,40,40);
-ctx.fillRect (200,200,40,40);
-ctx.fillRect (200,280,40,40);
-ctx.fillRect (200,360,40,40);
-
-ctx.fillRect (240,0,40,40); 
-ctx.fillRect (240,80,40,40,)
-ctx.fillRect(240,160,40,40);
-ctx.fillRect (240,240,40,40);
-ctx.fillRect (240,320,40,41);
-
-
-
-
-
+for (let i = 0; i < 400; i += 120) {
+    for (let j = 0; j < 600; j += 120) {
+        drawSquare(j + 60, i + 60, 60, 60, "black");
+    }
+}
