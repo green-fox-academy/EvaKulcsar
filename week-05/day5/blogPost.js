44f35501e435be225be6d3a0606cca4847036e02
Éva <kulcsareva@gmail.com>
2020-12-06 16:16:10 +0100

    blogPost

--- week-05/day5/blogPost.js
+++ week-05/day5/blogPost.js
@@ -13,3 +13,39 @@
 //Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. 
 //When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t
 //really into the whole organizer profile thing.
+ 
+class BlogPost {
+    constructor (authorName,title,text,publicationDate){
+        this.authorName = authorName;
+        this.title = title;
+        this.text = text;
+        this.publicationDate = publicationDate;
+    }
+} 
+
+console.log(
+    new BlogPost (
+        "Gipsz Jakab",
+        "Lorem Ipsum",
+        "Lorem ipsum dolor sit amet.",
+        "2000.05.04." 
+    )
+);
+
+console.log(
+    new BlogPost(
+        "Tim Urban",
+        "Wait but why",
+        "A popular long-form, stick-figure-illustrated blog about almost everything.",
+        "2010.10.10."
+    )
+);
+
+console.log(
+    new BlogPost(
+        "William",
+        "One Engineer Is Trying to Get IBM to Reckon With Trump",
+        "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.",
+        "2017.03.28."
+    )
+);
