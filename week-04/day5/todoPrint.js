4f1bfe50c2b32cd3ee2df3f049c771259d5b7fcc
Éva <kulcsareva@gmail.com>
2020-12-13 01:32:57 +0100

    todoPrint

---
+++ week-04/day5/todoPrint.js
@@ -0,0 +1,19 @@
+//A "todoText" lista kezdődjön a "My todos:" szöveggel (a jelenlegihez add hozzá ezt a szöveget úgy, hogy 
+   // elé kerüljön)
+//Add hozzá a " - Download games" tennivalót a lista végére
+//Add hozzá a " - Diablo" tennivalót a lista végére, 2 szóköz indentációval
+//Várt eredmény:
+//My todos:
+// - Buy milk
+ //- Download games
+    // - Diablo
+
+
+    let todoText = " - Buy milk\n";
+
+        todoText = "My todos:\n" + todoText + " - Download games\n" +  "    - Diablo";
+        
+
+
+    console.log(todoText);
+
