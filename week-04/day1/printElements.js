d493646d0e6ebbd881a544ab7487709377d5c2a6
Éva <kulcsareva@gmail.com>
2020-12-13 23:14:04 +0100

    printElement

--- week-04/day1/printElements.js
+++ week-04/day1/printElements.js
@@ -11,4 +11,14 @@ const numbers = [4, 5, 6, 7];
 
 //numbers.forEach(numbers => {console.log(numbers)});
 
+console.log ("1. megoldás");
 numbers.forEach(numbers => console.log(numbers));
+
+console.log ("2. megoldás");
+for (let i=0; i < numbers.length; i++){
+    console.log (numbers[i]);
+
+}
+
+console.log ("3. megoldás");
+numbers.map((element)=>console.log(element));
