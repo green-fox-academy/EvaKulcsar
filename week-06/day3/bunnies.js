79b6f8df9aff82db0872788f565c028a33f6daa7
Éva <kulcsareva@gmail.com>
2020-12-18 00:47:51 +0100

    2answers of bunnies

--- week-06/day3/bunnies.js
+++ week-06/day3/bunnies.js
@@ -1,11 +1,23 @@
+"use strict"
+
 // Egy bizonyos számú nyuszikánk van és minden nyuszinak két nagy, lapát füle.
 // Ki szeretnénk számolni az összes nyuszifül számát rekurzívan
 // (ciklusok és szorzás nélkül).
 
 function Bunneis (head) {
-    if (head === 1){
-        return 2;
+    if (head === 0){
+        return 0;
     }
-    return Bunneis (head -1) + 2 ;
+    return (Bunneis(head -1) + 2) ;
 }
-console.log(Bunneis(3));
+console.log(Bunneis(9));
+//vagy
+
+function getEarsSum(n) {
+    if ( n === 0) {
+        return 0;
+    }
+    return (2 + getEarsSum(n - 1));
+}
+
+console.log(getEarsSum(9));
