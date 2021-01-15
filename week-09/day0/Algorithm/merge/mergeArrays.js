c25b0f390ef652dba2266542eef434c462041a23
Éva <kulcsareva@gmail.com>
2021-01-15 15:20:24 +0100

    mergeArrays

--- week-09/day0/Algorithm/merge/mergeArrays.js
+++ week-09/day0/Algorithm/merge/mergeArrays.js
@@ -10,25 +10,12 @@
 //["apple", "dog", "pear", "cat", "banana", "orange"]
 
 
-function mergeElements(...args) {
-    const total = [];
-    for (let i = 0; i < Math.max(args[0].length, args[1].length) * 2; i++) {
-      if (i % 2 && args[1][Math.floor(i / 2)] !== undefined) {
-        total.push(args[1][Math.floor(i / 2)]);
-      } else if (i % 2 === 0 && args[0][i / 2] !== undefined) {
-        total.push(args[0][i / 2]);
-      }
-    }
-    return total;
-  }
+function mergeElements(arr1,arr2) {
+
+
+
+
+
+
   
-  const fruits = ['kiwi', 'banana', 'raspberry', 'lime'];
-  const animal = ['dog', 'cat', 'hippo'];
-  
-  console.log(mergeElements(fruits, animal));
-  
-  const merged = mergeElements(
-    ['apple', 'pear', 'banana', 'orange'],
-    ['dog', 'cat']
-  );
-  console.log(merged);
+}
